108cba1488c8ef74f9e46e248b3f105795006299
romanbrancato <romanbrancato88@gmail.com>
2025-03-24 18:23:44 -0400

    script now has to wait for match to finish,cropped attack button to work with star bonus

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ from time import sleep
 from detection import *
 
 ATTACK_BUTTON = (60, 475)
-HERO_SLOT = (95, 485)
-FIRST_TROOP_SLOT = (180, 485)
+FIRST_TROOP_SLOT = (100, 485)
 GRASS = (730, 85)
 TROOP_SLOTS = 6
 
@@ -13,28 +12,37 @@ class Bot:
     def __init__(self, client):
         self.client = client
 
-    def await_images(self, image_name):
+    def await_images(self, image_name, confidence=0.9, delay=0.2):
         while True:
-            found = locate_image(self.client.capture_screen(), image_name, 0.9)
+            found = locate_image(self.client.capture_screen(), image_name, confidence)
             if found: return found
-            sleep(0.1)
+            sleep(delay)
 
     def handle_matching(self):
-        while True:
-            self.client.device.click(*ATTACK_BUTTON)
-            sleep(0.2)
-            if match := locate_image(self.client.capture_screen(), ["match.png"], 0.9):
-                self.client.device.click(*match)
-                self.await_images(["match_begin.png"])
-                return
+        self.client.device.click(*ATTACK_BUTTON)
+        match = self.await_images(["match.png"], delay=1)
+        if match:
+            self.client.device.click(*match)
+            self.await_images(["match_begin.png"])
+            return
 
     def handle_battle(self):
-        self.client.device.click(*HERO_SLOT)
-        self.client.device.click(*GRASS)
         self.client.device.click(*FIRST_TROOP_SLOT)
         for _ in range(TROOP_SLOTS):
             self.client.device.click(*GRASS)
         sleep(1)  # CANNOT BE LOWER AS TROOPS WON'T REGISTER AS DEPLOYED
+
+    # def handle_surrender(self):
+    #     if surrender := locate_image(self.client.capture_screen(),["surrender.png"], 0.6):
+    #         self.client.device.click(*surrender)
+    #     sleep(0.2)
+    #     if surrender_confirm := locate_image(self.client.capture_screen(), ["surrender_confirm.png"], 0.6):
+    #         self.client.device.click(*surrender_confirm)
+    #     sleep(0.5)
+    #     if return_home := locate_image(self.client.capture_screen(), ["return_home.png"], 0.6):
+    #         self.client.device.click(*return_home)
+    #         self.await_images(["attack.png"])
+    #         return
 
     def handle_restart(self):
         self.client.device.app_stop('com.supercell.clashofclans')
